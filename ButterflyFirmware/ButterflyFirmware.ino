9794bd170ee4e096de88b8b62333a0723af8c60b
strofos <pirvu.stefan@gmail.com>
2020-06-18 14:34:07 +0300

    Update ButterflyFirmware.ino

--- ButterflyFirmware/ButterflyFirmware.ino
+++ ButterflyFirmware/ButterflyFirmware.ino
@@ -1,4 +1,4 @@
-#define LEDS_TOTAL 36
+#define LED_SPRITES 36
 
 #include <avr/power.h>
 #include <avr/sleep.h>
@@ -6,7 +6,7 @@
 
 #define PIN_ON 4
 
-uint8_t leds[LEDS_TOTAL] = { 
+uint8_t ledSprite[LED_SPRITES] = { 
   B1110, B1101, B1110, B1101,
   B1110, B1101, B1011, B0111,
   B1011, B0111, B1011, B0111, 
@@ -20,6 +20,19 @@ uint8_t leds[LEDS_TOTAL] = {
   B0011, B0110, B1001, B0101
 };
 
+#define LEDS_TOTAL 12
+uint8_t led[LEDS_TOTAL] = {
+  B0010, B0001, B0100, B0100,
+  B1000, B1000, B0001, B0001, 
+  B0010, B0010, B1000, B0100 
+};
+uint8_t ledMask[LEDS_TOTAL] = {
+  B0011, B0011, B0101, B0110,
+  B1001, B1010, B0101, B1001,
+  B0110, B1010, B1100, B1100,
+};
+
+
 void setup() {
   // set pins as outputs
   //DDRB = (1 << PB0) | (1 << PB1) | (1 << PB2) | (1 << PB3);
@@ -27,32 +40,80 @@ void setup() {
   pinMode(1 ,OUTPUT);
   pinMode(2 ,OUTPUT);
   pinMode(3 ,OUTPUT);
-  pinMode(4, INPUT_PULLUP);
+  pinMode(4, INPUT);
+
 }
 
+void ledON(uint8_t idx) {
+    DDRB = ledMask[idx];
+    PORTB = led[idx];
+}
+void ledON(uint8_t idx, int duration) {
+    ledON(idx);
+    delay(duration);
+}
 
-uint8_t loopCounter = 0;
+void ledON_us(uint8_t idx, int duration) {
+    ledON(idx);
+    _delay_us(duration);
+}
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  for(uint8_t idx = 0; idx < LEDS_TOTAL; idx++){
-    PORTB = leds[idx];
+void rotate(int spd) {
+  ledON(1, spd);
+  ledON(3, spd);
+  ledON(9, spd);
+  ledON(5, spd);
+  ledON(7, spd);
+  ledON(6, spd);
+  ledON(0, spd);
+  ledON(8, spd);
+  ledON(4, spd);
+  ledON(2, spd);
+}
+
+void rotate_us(int spd_ms) {
+  ledON_us(1, spd_ms);
+  ledON_us(3, spd_ms);
+  ledON_us(9, spd_ms);
+  ledON_us(5, spd_ms);
+  ledON_us(7, spd_ms);
+  ledON_us(6, spd_ms);
+  ledON_us(0, spd_ms);
+  ledON_us(8, spd_ms);
+  ledON_us(4, spd_ms);
+  ledON_us(2, spd_ms);
+}
+
+void multiBlink() {
+  for(uint8_t idx = 0; idx < LED_SPRITES; idx++){
+    PORTB = ledSprite[idx];
     delay(10);
   }
-  
+}
+
+unsigned long loopCounter = 0;
+
+void loop() {
+  for(uint8_t idx = 10; idx > 1; idx--){ rotate(idx); }
+  for(uint8_t idx = 500; idx > 300; idx-=100){ rotate_us(idx); }
+  rotate_us(100);
+  rotate_us(100);
+
+  DDRB = B1111;
+
+  for(uint8_t idx = 10; idx > 1; idx--){ multiBlink(); }
+
   loopCounter++;
 
-  if (loopCounter % 5 == 0){
-    // sleep
-    PORTB = 0x00;
-    for(uint16_t idx = 0; idx < 120; idx++){
-      goToSleep(); // each sleep takes 8sec
-    }
-    loopCounter = 0;
+  // sleep
+  PORTB = 0x00;
+  for(uint16_t idx = 0; idx < loopCounter; idx++){
+    goToSleep(); // each sleep takes 8sec
   }
 }
 
 void goToSleep() {
+  pinMode(4, INPUT);
 
   /* Clear the reset flag. */
   MCUSR &= ~(1<<WDRF);
